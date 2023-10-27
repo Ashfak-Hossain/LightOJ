4f95714ad5d7455d288a6c5b52d53a6fd178aef7
Berlin <evan1234.ek@gmail.com>
2023-10-27 14:17:56 +0600

    solved

---
+++ Mad_Counting.cpp
@@ -0,0 +1,94 @@
+/**
+ *        Bismillahir Rahmanir Rahim
+ *        Author:  Ashfak Hossain Evan, American International University, Bangladesh
+ *        Created: 27/10/2023 14:11:17
+ **/
+#include <bits/stdc++.h>
+
+#ifdef BERLIN
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((long long)x.size())
+#define char2Int(c) (c - '0')
+#define lastEle(vec) vec[vec.size() - 1]
+#define PI 3.1415926535897932384626433832795l
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define range(i, v) for (auto &i : v)
+#define fix(prec) cout << setprecision(prec) << fixed;
+#define mem(arr, v) memset(arr, v, sizeof(arr))
+#define getunique(v)                      \
+    {                                     \
+        sort(all(v));                     \
+        v.erase(unique(all(v)), v.end()); \
+    }
+#define Muichiro ios_base::sync_with_stdio(false);
+#define Tokito     \
+    cin.tie(NULL); \
+    cout.tie(NULL);
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve(int cs)
+{
+    int n;
+    cin >> n;
+
+    map<int, int> mp;
+    for (int i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        mp[x]++;
+    }
+
+    int ans = 0;
+
+    range(i, mp)
+    {
+        ans += ((i.s / (i.f + 1)) + (i.s % (i.f + 1) != 0 ? 1 : 0)) * (i.f + 1);
+    }
+
+    cout << "Case " << cs << ": ";
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    Muichiro Tokito
+
+#ifdef BERLIN
+        freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int _;
+    cin >> _;
+    for (int i = 1; i <= _; i++)
+        solve(i);
+
+    return 0;
+}
